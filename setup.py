fac4a885d83e3935cc3b28e6b1d936e217da4e75
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-08-16 17:59:05 +0200

    fix indentation for "setup()" parameters in setup.py

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@
 from setuptools import setup, find_packages
 
 setup(
-      name='YouTubeImportMC',
-      version='0.6',
-
-      author='Felix Schwarz',
-      author_email='info@schwarz.eu',
-      license='GPL v3 or later', # see LICENSE.txt
-        
-      packages=find_packages(),
-      namespace_packages = ['mcore'],
-      include_package_data=True,
-      entry_points = {
-          'mediacore.plugin': [
-              'youtube_import = mcore.youtube_import.mediacore_plugin',
-          ],
-      },
-      message_extractors = {'mcore/youtube_import': [
-          ('**.py', 'python', None),
-          ('templates/**.html', 'genshi', {'template_class': 'genshi.template.markup:MarkupTemplate'}),
-      ]},
+    name='YouTubeImportMC',
+    version='0.6',
+    
+    author='Felix Schwarz',
+    author_email='info@schwarz.eu',
+    license='GPL v3 or later', # see LICENSE.txt
+    
+    packages=find_packages(),
+    namespace_packages = ['mcore'],
+    include_package_data=True,
+    entry_points = {
+        'mediacore.plugin': [
+            'youtube_import = mcore.youtube_import.mediacore_plugin',
+        ],
+    },
+    message_extractors = {'mcore/youtube_import': [
+        ('**.py', 'python', None),
+        ('templates/**.html', 'genshi', {'template_class': 'genshi.template.markup:MarkupTemplate'}),
+    ]},
 )
 
