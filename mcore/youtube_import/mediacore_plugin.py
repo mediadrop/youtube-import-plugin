b00f1668a6f24ecc46b356c01d5914cea86aee2c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2012-08-16 15:16:36 +0200

    add license header

--- mcore/youtube_import/mediacore_plugin.py
+++ mcore/youtube_import/mediacore_plugin.py
@@ -1,4 +1,8 @@
 # -*- coding: UTF-8 -*-
+# This file is a part of the YouTube Import plugin for MediaCore CE.
+# The source code contained in this file is licensed under the GPL v3 (or at 
+# your option any later version).
+# See LICENSE.txt in the main project directory for more information.
 
 # Register admin hooks
 import mcore.youtube_import.admin
