64ebda11bcd0a07fc33690bba5c6806567732e5a
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2013-05-09 11:02:52 +0200

    add license header to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# This file is a part of the YouTube Import plugin for MediaCore CE.
+# The source code contained in this file is licensed under the GPL v3 (or at 
+# your option any later version).
+# See LICENSE.txt in the main project directory for more information.
 
 import sys
 
